f099085e16385474fdcabed9af0cb3f2df22eb05
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-10-17 15:09:24 +0900

    chat-space_user_devise5

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
 
   def index
   end
@@ -11,7 +12,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, notice: "グループを作成しました"
+      redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
     end
@@ -21,10 +22,20 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, {:user_ids => []})
   end
+
+  def set_group
+    @group = Group.find(parmas[:id])
+  end
 end
+
