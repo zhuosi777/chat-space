5e1cea2670b8ad0a86e707dfbbb4369c357544cf
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-10-18 15:07:38 +0900

    chat-space_form2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    @user = @group.users
   end
 
   def create
