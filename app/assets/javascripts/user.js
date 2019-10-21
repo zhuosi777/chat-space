71f306a1e0e465b1ebdc2bcd3b0aae648219f4ec
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-10-21 21:51:31 +0900

    chat-space-step-seach2

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,21 +2,27 @@ $(document).on("turbolinks:load", function () {
 
   var search_list = $("#user-search-result");
 
+
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">
-                    ${user.name}
-                  </p>
+                  <p class="chat-group-user__name">${user.name}</p>
                     <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
                     </div>`;
     search_list.append(html);
   }
 
+  function appendMembers(name, user_id) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value=${user_id}>
+                  <a class='chat-group-user__name'>${name}</a>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`;
+    $(".js-add-user").append(html);
+  }
+
   function appendErrMsgToHTML(msg) {
     var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">
-                  ${msg}
-                  </p>
+                  <p class="chat-group-user__name">${msg}</p>
                 </div>`;
     search_list.append(html);
   }
@@ -32,11 +38,10 @@ $(document).on("turbolinks:load", function () {
     })
 
       .done(function (users) {
+        $("#user-search-result").empty();
         if (input.length === 0) {
-          $("#user-search-result").empty();
         }
         else if (input.length !== 0) {
-          $("#user-search-result").empty();
           users.forEach(function (user) {
             appendUser(user);
           });
@@ -49,5 +54,17 @@ $(document).on("turbolinks:load", function () {
       .fail(function () {
         alert("ユーザーの検索に失敗しました");
       })
+  });
+
+  $(document).on("click", ".chat-group-user__btn--add", function () {
+
+    var name = $(this).data("user-name");
+    var user_id = $(this).data("user-id");
+    $(this).parent().remove();
+    appendMembers(name, user_id);
+  });
+
+  $(document).on("click", ".user-search-remove", function () {
+    $(this).parent().remove();
   });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where("name LIKE(?)", "%#{params[:keyword]}%")
+    @users = User.where("name LIKE(?) and (id!=?)", "%#{params[:keyword]}%","#{current_user.id}").where.not(id:params[:user_ids])
     respond_to do |format|
       format.html
       format.json
