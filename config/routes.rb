4822f698480f8fb45040b011f661df250985df07
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-10-23 15:47:18 +0900

    chat-space-step-auto1

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create, :edit]
+    resources :messages, only: [:index, :create]
+
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: "json" }
+    end
   end
 end
