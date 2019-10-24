4fe1673a1aa55aa67f207ecc2ad4dc5a2b6d8121
zhuosi777 <zhuosi777@yahoo.co.jp>
2019-10-24 19:48:47 +0900

    chat-space_ver1.16

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.2"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:zhuosi777/chat-space.git"
+set :repo_url, "git@github.com:zhuosi777/chat-space.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
